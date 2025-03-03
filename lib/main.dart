e2a78828e92eeca0f1610b966eac85530a4639a3
RenoVISION <poul2002.02@gmail.com>
2025-03-02 22:22:56 -0500

    another class for scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'screens/bluetooth_off_screen.dart';
 import 'screens/scan_screen.dart';
-import 'render.dart';
+import 'package:fire_vision/render.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
@@ -49,35 +49,43 @@ class _FlutterBlueAppState extends State<MyApp> {
     return MaterialApp(
       color: const Color.fromARGB(255, 190, 98, 12),
       title: 'FireVisions',
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: const Text("FireVision"),
-        // ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/images/icons8-fire-100.png',
-                  height: 200, scale: 2),
-                ElevatedButton(
-                  onPressed: () {
-                    print('Navigating to Render'); // Debugging print statement
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const Render(),
-                      ),
-                      
-                    );
-                    print('Did it push?');
-                  },
-                  child: const Text('Back'),
-              ),
-              Expanded(child: screen),
-            ],
-          ),
+      home: HomeScreen(screen: screen),
+      navigatorObservers: [BluetoothAdapterStateObserver()],
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final Widget screen;
+
+  const HomeScreen({Key? key, required this.screen}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("FireVision"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Image.asset('assets/images/icons8-fire-100.png',
+                height: 200, scale: 2),
+            ElevatedButton(
+              onPressed: () {
+                print('Navigating to Render'); // Debugging print statement
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const Render(),
+                  ),
+                );
+              },
+              child: const Text('3D Render'),
+            ),
+            Expanded(child: screen),
+          ],
         ),
       ),
-      navigatorObservers: [BluetoothAdapterStateObserver()],
     );
   }
 }
@@ -109,30 +117,17 @@ class BluetoothAdapterStateObserver extends NavigatorObserver {
   }
 }
 
-// //import 'dart:convert';
-
 // import 'dart:async';
 // import 'screens/bluetooth_off_screen.dart';
 // import 'screens/scan_screen.dart';
-// import 'package:fire_vision/Render.dart';
-// // import 'package:english_words/english_words.dart';
-// // import 'package:provider/provider.dart';
-
+// import 'render.dart';
 // import 'package:flutter/material.dart';
-// //import 'package:flutter/services.dart';
 // import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-// //import 'package:permission_handler/permission_handler.dart';
-
-// //void main() => runApp(const MyApp());
 
 // void main() {
 //   FlutterBluePlus.setLogLevel(LogLevel.verbose, color: true);
 //   runApp(const MyApp());
 // }
-
-// // This widget shows BluetoothOffScreen or
-// // ScanScreen depending on the adapter state
-// //
 
 // class MyApp extends StatefulWidget {
 //   const MyApp({super.key});
@@ -140,13 +135,6 @@ class BluetoothAdapterStateObserver extends NavigatorObserver {
 //   @override
 //   State<MyApp> createState() => _FlutterBlueAppState();
 // }
-
-// // class ElevatedButtonExample extends StatefulWidget {
-// //   const ElevatedButtonExample({super.key});
-
-// //   @override
-// //   State<ElevatedButtonExample> createState() => _FlutterBlueAppState();
-// // }
 
 // class _FlutterBlueAppState extends State<MyApp> {
 //   BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
@@ -181,24 +169,25 @@ class BluetoothAdapterStateObserver extends NavigatorObserver {
 //       color: const Color.fromARGB(255, 190, 98, 12),
 //       title: 'FireVisions',
 //       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text("FireVision"),
-//         ),
+
 //         body: Center(
 //           child: Column(
 //             children: <Widget>[
 //               Image.asset('assets/images/icons8-fire-100.png',
 //                   height: 200, scale: 2),
-//               ElevatedButton(
-//                 onPressed: () {
-//                   print('Navigating to Render'); // Debugging print statement
-//                   Navigator.of(context).push(
-//                     MaterialPageRoute(
-//                       builder: (context) => const Render(),
-//                     ),
-//                   );
-//                 },
-//                 child: const Text('3D Render'),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     print('Navigating to Render'); // Debugging print statement
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => const Render(),
+//                       ),
+
+//                     );
+//                     //print('Did it push?');
+//                   },
+//                   child: const Text('Back'),
 //               ),
 //               Expanded(child: screen),
 //             ],
@@ -236,3 +225,131 @@ class BluetoothAdapterStateObserver extends NavigatorObserver {
 //     _adapterStateSubscription = null;
 //   }
 // }
+
+// // //import 'dart:convert';
+
+// // import 'dart:async';
+// // import 'screens/bluetooth_off_screen.dart';
+// // import 'screens/scan_screen.dart';
+// // import 'package:fire_vision/Render.dart';
+// // // import 'package:english_words/english_words.dart';
+// // // import 'package:provider/provider.dart';
+
+// // import 'package:flutter/material.dart';
+// // //import 'package:flutter/services.dart';
+// // import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+// // //import 'package:permission_handler/permission_handler.dart';
+
+// // //void main() => runApp(const MyApp());
+
+// // void main() {
+// //   FlutterBluePlus.setLogLevel(LogLevel.verbose, color: true);
+// //   runApp(const MyApp());
+// // }
+
+// // // This widget shows BluetoothOffScreen or
+// // // ScanScreen depending on the adapter state
+// // //
+
+// // class MyApp extends StatefulWidget {
+// //   const MyApp({super.key});
+
+// //   @override
+// //   State<MyApp> createState() => _FlutterBlueAppState();
+// // }
+
+// // // class ElevatedButtonExample extends StatefulWidget {
+// // //   const ElevatedButtonExample({super.key});
+
+// // //   @override
+// // //   State<ElevatedButtonExample> createState() => _FlutterBlueAppState();
+// // // }
+
+// // class _FlutterBlueAppState extends State<MyApp> {
+// //   BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
+
+// //   late StreamSubscription<BluetoothAdapterState> _adapterStateStateSubscription;
+
+// //   @override
+// //   void initState() {
+// //     super.initState();
+// //     _adapterStateStateSubscription =
+// //         FlutterBluePlus.adapterState.listen((state) {
+// //       _adapterState = state;
+// //       if (mounted) {
+// //         setState(() {});
+// //       }
+// //     });
+// //   }
+
+// //   @override
+// //   void dispose() {
+// //     _adapterStateStateSubscription.cancel();
+// //     super.dispose();
+// //   }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     Widget screen = _adapterState == BluetoothAdapterState.on
+// //         ? const ScanScreen()
+// //         : BluetoothOffScreen(adapterState: _adapterState);
+
+// //     return MaterialApp(
+// //       color: const Color.fromARGB(255, 190, 98, 12),
+// //       title: 'FireVisions',
+// //       home: Scaffold(
+// //         appBar: AppBar(
+// //           title: const Text("FireVision"),
+// //         ),
+// //         body: Center(
+// //           child: Column(
+// //             children: <Widget>[
+// //               Image.asset('assets/images/icons8-fire-100.png',
+// //                   height: 200, scale: 2),
+// //               ElevatedButton(
+// //                 onPressed: () {
+// //                   print('Navigating to Render'); // Debugging print statement
+// //                   Navigator.of(context).push(
+// //                     MaterialPageRoute(
+// //                       builder: (context) => const Render(),
+// //                     ),
+// //                   );
+// //                 },
+// //                 child: const Text('3D Render'),
+// //               ),
+// //               Expanded(child: screen),
+// //             ],
+// //           ),
+// //         ),
+// //       ),
+// //       navigatorObservers: [BluetoothAdapterStateObserver()],
+// //     );
+// //   }
+// // }
+
+// // class BluetoothAdapterStateObserver extends NavigatorObserver {
+// //   StreamSubscription<BluetoothAdapterState>? _adapterStateSubscription;
+
+// //   @override
+// //   void didPush(Route route, Route? previousRoute) {
+// //     super.didPush(route, previousRoute);
+// //     if (route.settings.name == '/DeviceScreen') {
+// //       // Start listening to Bluetooth state changes when a new route is pushed
+// //       _adapterStateSubscription ??=
+// //           FlutterBluePlus.adapterState.listen((state) {
+// //         if (state != BluetoothAdapterState.on) {
+// //           // Pop the current route if Bluetooth is off
+// //           navigator?.pop();
+// //         }
+// //       });
+// //     }
+// //   }
+
+// //   @override
+// //   void didPop(Route route, Route? previousRoute) {
+// //     super.didPop(route, previousRoute);
+// //     // Cancel the subscription when the route is popped
+// //     _adapterStateSubscription?.cancel();
+// //     _adapterStateSubscription = null;
+// //   }
+// // }
