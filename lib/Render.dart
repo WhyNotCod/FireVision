12dc55d59ece5279f1e3cbb5ae07f00304ba4a37
RenoVISION <poul2002.02@gmail.com>
2025-03-02 20:29:56 -0500

    simple render

--- lib/Render.dart
+++ lib/Render.dart
@@ -1,101 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cube/flutter_cube.dart';
 
 class Render extends StatefulWidget {
-  const Render({super.key});
+  const Render({super.key, this.title});
 
-  //final String? title;
+  final String? title;
 
   @override
   State<Render> createState() => _RenderState();
 }
 
-class _RenderState extends State<Render> with SingleTickerProviderStateMixin {
-  late Scene _scene;
-  Object? _cube;
-  late AnimationController _controller;
-
-  Object? _c;
-  void _onSceneCreated(Scene scene) async {
-    _scene = scene;
-    scene.camera.position.z = 50;
-    _cube = Object(
-        scale: Vector3(5.0, 5.0, 5.0),
-        backfaceCulling: true,
-        fileName: 'assets/file.obj');
-    _c = Object(
-        scale: Vector3(5.0, 5.0, 5.0),
-        backfaceCulling: true,
-        fileName: 'assets/file.obj');
-    _cube!.add(_c!);
-
-    scene.world.add(_cube!);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _RenderState extends State<Render> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Fire_3D"),
+        title: Text(widget.title ?? '3D Render Page'),
       ),
-      body: Stack(
-        children: [
-          Center(
-            child: GestureDetector(
-              onPanUpdate: (details) {
-                if (_cube != null) {
-                  _cube!.rotation.x += details.delta.dy / 100;
-                  _cube!.rotation.y += details.delta.dx / 100;
-                  _cube!.updateTransform();
-                  _scene.update();
-                }
-              },
-              child: Cube(onSceneCreated: _onSceneCreated),
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Back'),
-            ),
-          ),
-        ],
+      body: Center(
+        child: const Text('Hello'),
       ),
     );
   }
 }
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _controller = AnimationController(
-  //       duration: const Duration(microseconds: 30000), vsync: this)
-  //     ..addListener(() {
-  //       if (_cube != null) {
-  //         _cube!.rotation.y = _controller.value * 360;
-  //         _cube!.updateTransform();
-  //         _scene.update();
-  //       }
-  //     })
-  //     ..repeat();
-  // }
+// import 'package:flutter/material.dart';
+// import 'package:flutter_cube/flutter_cube.dart';
 
-  
 // class Render extends StatefulWidget {
-//   const Render({super.key, this.title});
+//   const Render({super.key});
 
-//   final String? title;
+//   //final String? title;
 
 //   @override
 //   State<Render> createState() => _RenderState();
@@ -106,33 +41,21 @@ class _RenderState extends State<Render> with SingleTickerProviderStateMixin {
 //   Object? _cube;
 //   late AnimationController _controller;
 
-//   void _onSceneCreated(Scene scene) {
+//   Object? _c;
+//   void _onSceneCreated(Scene scene) async {
 //     _scene = scene;
 //     scene.camera.position.z = 50;
 //     _cube = Object(
 //         scale: Vector3(5.0, 5.0, 5.0),
 //         backfaceCulling: true,
 //         fileName: 'assets/file.obj');
-//     _cube!.add(Object(
+//     _c = Object(
 //         scale: Vector3(5.0, 5.0, 5.0),
 //         backfaceCulling: true,
-//         fileName: 'assets/file.obj'));
-//     scene.world.add(_cube!);
-//   }
+//         fileName: 'assets/file.obj');
+//     _cube!.add(_c!);
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller = AnimationController(
-//         duration: const Duration(microseconds: 30000), vsync: this)
-//       ..addListener(() {
-//         if (_cube != null) {
-//           _cube!.rotation.y = _controller.value * 360;
-//           _cube!.updateTransform();
-//           _scene.update();
-//         }
-//       })
-//       ..repeat();
+//     scene.world.add(_cube!);
 //   }
 
 //   @override
@@ -146,32 +69,135 @@ class _RenderState extends State<Render> with SingleTickerProviderStateMixin {
 //     return Scaffold(
 //       appBar: AppBar(
 //         centerTitle: true,
-//         title: Text(widget.title!),
+//         title: Text("Fire_3D"),
 //       ),
-//       body: Center(
-//         child: GestureDetector(
-//           onPanUpdate: (details) {
-//             if (_cube != null) {
-//               _cube!.rotation.x += details.delta.dy / 100;
-//               _cube!.rotation.y += details.delta.dx / 100;
-//               _cube!.updateTransform();
-//               _scene.update();
-//             }
-//           },
-//           child: Cube(onSceneCreated: _onSceneCreated),
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           Navigator.pop(context);
-//         },
-//         child: const Icon(Icons.arrow_back),
+//       body: Stack(
+//         children: [
+//           Center(
+//             child: GestureDetector(
+//               onPanUpdate: (details) {
+//                 if (_cube != null) {
+//                   _cube!.rotation.x += details.delta.dy / 100;
+//                   _cube!.rotation.y += details.delta.dx / 100;
+//                   _cube!.updateTransform();
+//                   _scene.update();
+//                 }
+//               },
+//               child: Cube(onSceneCreated: _onSceneCreated),
+//             ),
+//           ),
+//           Positioned(
+//             bottom: 20,
+//             right: 20,
+//             child: ElevatedButton(
+//               onPressed: () {
+//                 Navigator.pop(context);
+//               },
+//               child: const Text('Back'),
+//             ),
+//           ),
+//         ],
 //       ),
 //     );
 //   }
 // }
 
+//   // @override
+//   // void initState() {
+//   //   super.initState();
+//   //   _controller = AnimationController(
+//   //       duration: const Duration(microseconds: 30000), vsync: this)
+//   //     ..addListener(() {
+//   //       if (_cube != null) {
+//   //         _cube!.rotation.y = _controller.value * 360;
+//   //         _cube!.updateTransform();
+//   //         _scene.update();
+//   //       }
+//   //     })
+//   //     ..repeat();
+//   // }
 
-// //import 'dart:async';
+  
+// // class Render extends StatefulWidget {
+// //   const Render({super.key, this.title});
+
+// //   final String? title;
+
+// //   @override
+// //   State<Render> createState() => _RenderState();
+// // }
+
+// // class _RenderState extends State<Render> with SingleTickerProviderStateMixin {
+// //   late Scene _scene;
+// //   Object? _cube;
+// //   late AnimationController _controller;
+
+// //   void _onSceneCreated(Scene scene) {
+// //     _scene = scene;
+// //     scene.camera.position.z = 50;
+// //     _cube = Object(
+// //         scale: Vector3(5.0, 5.0, 5.0),
+// //         backfaceCulling: true,
+// //         fileName: 'assets/file.obj');
+// //     _cube!.add(Object(
+// //         scale: Vector3(5.0, 5.0, 5.0),
+// //         backfaceCulling: true,
+// //         fileName: 'assets/file.obj'));
+// //     scene.world.add(_cube!);
+// //   }
+
+// //   @override
+// //   void initState() {
+// //     super.initState();
+// //     _controller = AnimationController(
+// //         duration: const Duration(microseconds: 30000), vsync: this)
+// //       ..addListener(() {
+// //         if (_cube != null) {
+// //           _cube!.rotation.y = _controller.value * 360;
+// //           _cube!.updateTransform();
+// //           _scene.update();
+// //         }
+// //       })
+// //       ..repeat();
+// //   }
+
+// //   @override
+// //   void dispose() {
+// //     _controller.dispose();
+// //     super.dispose();
+// //   }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         centerTitle: true,
+// //         title: Text(widget.title!),
+// //       ),
+// //       body: Center(
+// //         child: GestureDetector(
+// //           onPanUpdate: (details) {
+// //             if (_cube != null) {
+// //               _cube!.rotation.x += details.delta.dy / 100;
+// //               _cube!.rotation.y += details.delta.dx / 100;
+// //               _cube!.updateTransform();
+// //               _scene.update();
+// //             }
+// //           },
+// //           child: Cube(onSceneCreated: _onSceneCreated),
+// //         ),
+// //       ),
+// //       floatingActionButton: FloatingActionButton(
+// //         onPressed: () {
+// //           Navigator.pop(context);
+// //         },
+// //         child: const Icon(Icons.arrow_back),
+// //       ),
+// //     );
+// //   }
+// // }
+
+
+// // //import 'dart:async';
 
 
