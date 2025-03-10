834ad87e473fa31ee55bd352f7ad03addfd2ffc7
RenoVISION <poul2002.02@gmail.com>
2025-03-09 21:45:37 -0400

    simple works

--- lib/line_3d.dart
+++ lib/line_3d.dart
@@ -6,64 +6,81 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_echarts/flutter_echarts.dart';
 
-class Graph extends StatelessWidget {
-  const Graph({super.key});
+// class Graph extends StatelessWidget {
+//   const Graph({super.key});
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('3D Line Chart'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Line3DChart(),
+//       ),
+//     );
+//   }
+// }
+
+// class Line3DChart extends StatelessWidget {
+//   const Line3DChart({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Step 1: Define variables
+//     // var x = 0;
+//     // var y = 2;
+//     // var z = 2; // Example value for z
+
+//     // Step 2: Define the data
+//     // var data = [
+//     //   [0, 0, 0], [1, 0, 0], // Line A-B
+//     //   [1, 0, 2], [0, 0, 2], [0, 0, 0],
+//     //   [0, 1, 0], [1, 1, 0], [1, 0, 0], [1, 0, 2],
+//     //   [1, 1, 2], [1, 1, 0], [0, 1, 0], [0, 1, 2],
+//     //   [0, 0, 2], [1, 0, 2], [1, 1, 2], [0, 1, 2]
+//     // ];
+
+//     // // Step 3: Modify the data list
+//     // for (var i = 0; i < data.length; i++) {
+//     //   for (var j = 0; j < data[i].length; j++) {
+//     //     if (data[i][j] == 2) {
+//     //       data[i][j] = z; // Replace 2 with z
+//     //     }
+//     //     if (j == 0) {
+//     //       // Update x-coordinate
+//     //       data[i][j] += x; // Add x to the x-coordinate
+//     //     }
+//     //     if (j == 1) {
+//     //       // Update y-coordinate
+//     //       data[i][j] += y; // Add y to the y-coordinate
+//     //     }
+//     //   }
+//     // }
+
+//     // Step 4: Convert data to a string to use in ECharts configuration
+//     //var dataString = data.toString().replaceAll('[', '').replaceAll(']', '');
+
+//     // Step 5: Define the ECharts option with the modified data
+
+//     // Step 6: Return ECharts widget
+//     // return (
+//     //   option: jsonEncode(option),
+//     // );
+//   }
+// }
+// import 'package:flutter/material.dart';
+// import 'package:flutter_echarts/flutter_echarts.dart';
+
+class Graph extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('3D Line Chart'),
+        title: Text('Simple Chart'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Line3DChart(),
-      ),
-    );
-  }
-}
-
-class Line3DChart extends StatelessWidget {
-  const Line3DChart({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Step 1: Define variables
-    // var x = 0;
-    // var y = 2;
-    // var z = 2; // Example value for z
-
-    // Step 2: Define the data
-    // var data = [
-    //   [0, 0, 0], [1, 0, 0], // Line A-B
-    //   [1, 0, 2], [0, 0, 2], [0, 0, 0],
-    //   [0, 1, 0], [1, 1, 0], [1, 0, 0], [1, 0, 2],
-    //   [1, 1, 2], [1, 1, 0], [0, 1, 0], [0, 1, 2],
-    //   [0, 0, 2], [1, 0, 2], [1, 1, 2], [0, 1, 2]
-    // ];
-
-    // // Step 3: Modify the data list
-    // for (var i = 0; i < data.length; i++) {
-    //   for (var j = 0; j < data[i].length; j++) {
-    //     if (data[i][j] == 2) {
-    //       data[i][j] = z; // Replace 2 with z
-    //     }
-    //     if (j == 0) {
-    //       // Update x-coordinate
-    //       data[i][j] += x; // Add x to the x-coordinate
-    //     }
-    //     if (j == 1) {
-    //       // Update y-coordinate
-    //       data[i][j] += y; // Add y to the y-coordinate
-    //     }
-    //   }
-    // }
-
-    // Step 4: Convert data to a string to use in ECharts configuration
-    //var dataString = data.toString().replaceAll('[', '').replaceAll(']', '');
-
-    // Step 5: Define the ECharts option with the modified data
-    return Container(child: Echarts(option: ''' {
+      body: Center(child: Container(child: Echarts(option: ''' {
       "tooltip": {},
       "backgroundColor": "#fff",
       "visualMap": {
@@ -106,47 +123,7 @@ class Line3DChart extends StatelessWidget {
           "lineStyle": {"width": 4}
         }
       ]
-    }'''));
-
-    // Step 6: Return ECharts widget
-    // return (
-    //   option: jsonEncode(option),
-    // );
+    }'''))),
+    );
   }
 }
-// import 'package:flutter/material.dart';
-// import 'package:flutter_echarts/flutter_echarts.dart';
-
-// class Graph extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Simple Chart'),
-//       ),
-//       body: Center(
-//         child: Container(
-//           child: Echarts(
-//             option: '''
-//               {
-//                 xAxis: {
-//                   type: 'category',
-//                   data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-//                 },
-//                 yAxis: {
-//                   type: 'value'
-//                 },
-//                 series: [{
-//                   data: [820, 932, 901, 934, 1290, 1330, 1320],
-//                   type: 'line'
-//                 }]
-//               }
-//             ''',
-//           ),
-//           width: 300,
-//           height: 250,
-//         ),
-//       ),
-//     );
-//   }
-// }
