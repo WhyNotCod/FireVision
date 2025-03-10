7240224d07ddf241f4e6f98586ebc761a908e563
RenoVISION <poul2002.02@gmail.com>
2025-03-09 22:05:24 -0400

    hey there, you're a rock star

--- lib/line_3d.dart
+++ lib/line_3d.dart
@@ -6,6 +6,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_echarts/flutter_echarts.dart';
 
+class Graph extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('3D Chart')),
+      body: Echarts(
+        extraScript: '''
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/echarts-gl/2.0.8/echarts-gl.min.js"></script>
+        ''',
+        option: '''
+          {
+            "tooltip": {},
+            "xAxis3D": {"type": "value"},
+            "yAxis3D": {"type": "value"},
+            "zAxis3D": {"type": "value"},
+            "grid3D": {"viewControl": {}},
+            "series": [{
+              "type": "line3D",
+              "data": [[0,0,0], [1,0,0], [1,0,2], [0,0,2]],
+              "lineStyle": {"width": 4}
+            }]
+          }
+        ''',
+      ),
+    );
+  }
+}
+
 // class Graph extends StatelessWidget {
 //   const Graph({super.key});
 
@@ -73,57 +101,57 @@ import 'package:flutter_echarts/flutter_echarts.dart';
 // import 'package:flutter/material.dart';
 // import 'package:flutter_echarts/flutter_echarts.dart';
 
-class Graph extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Simple Chart'),
-      ),
-      body: Center(child: Container(child: Echarts(option: ''' {
-      "tooltip": {},
-      "backgroundColor": "#fff",
-      "visualMap": {
-        "show": false,
-        "dimension": 2,
-        "min": 0,
-        "max": 30,
-        "inRange": {
-          "color": [
-            "#313695",
-            "#4575b4",
-            "#74add1",
-            "#abd9e9",
-            "#e0f3f8",
-            "#ffffbf",
-            "#fee090",
-            "#fdae61",
-            "#f46d43",
-            "#d73027",
-            "#a50026"
-          ]
-        }
-      },
-      "xAxis3D": {"type": "value", "min": -1.80, "max": 1.8},
-      "yAxis3D": {"type": "value", "min": 0, "max": 5.5},
-      "zAxis3D": {"type": "value", "min": 0, "max": 2.0},
-      "grid3D": {
-        "viewControl": {"projection": "orthographic"}
-      },
-      "series": [
-        {
-          "type": "line3D",
-          "data": [
-      [0, 0, 0], [1, 0, 0],
-      [1, 0, 2], [0, 0, 2], [0, 0, 0],
-      [0, 1, 0], [1, 1, 0], [1, 0, 0], [1, 0, 2],
-      [1, 1, 2], [1, 1, 0], [0, 1, 0], [0, 1, 2],
-      [0, 0, 2], [1, 0, 2], [1, 1, 2], [0, 1, 2]
-    ],
-          "lineStyle": {"width": 4}
-        }
-      ]
-    }'''))),
-    );
-  }
-}
+// class Graph extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Simple Chart'),
+//       ),
+//       body: Center(child: Container(child: Echarts(option: ''' {
+//       "tooltip": {},
+//       "backgroundColor": "#fff",
+//       "visualMap": {
+//         "show": false,
+//         "dimension": 2,
+//         "min": 0,
+//         "max": 30,
+//         "inRange": {
+//           "color": [
+//             "#313695",
+//             "#4575b4",
+//             "#74add1",
+//             "#abd9e9",
+//             "#e0f3f8",
+//             "#ffffbf",
+//             "#fee090",
+//             "#fdae61",
+//             "#f46d43",
+//             "#d73027",
+//             "#a50026"
+//           ]
+//         }
+//       },
+//       "xAxis3D": {"type": "value", "min": -1.80, "max": 1.8},
+//       "yAxis3D": {"type": "value", "min": 0, "max": 5.5},
+//       "zAxis3D": {"type": "value", "min": 0, "max": 2.0},
+//       "grid3D": {
+//         "viewControl": {"projection": "orthographic"}
+//       },
+//       "series": [
+//         {
+//           "type": "line3D",
+//           "data": [
+//       [0, 0, 0], [1, 0, 0],
+//       [1, 0, 2], [0, 0, 2], [0, 0, 0],
+//       [0, 1, 0], [1, 1, 0], [1, 0, 0], [1, 0, 2],
+//       [1, 1, 2], [1, 1, 0], [0, 1, 0], [0, 1, 2],
+//       [0, 0, 2], [1, 0, 2], [1, 1, 2], [0, 1, 2]
+//     ],
+//           "lineStyle": {"width": 4}
+//         }
+//       ]
+//     }'''))),
+//     );
+//   }
+// }
